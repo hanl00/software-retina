2e949800b1dcc1454211a298296840144c8a2461
nick.han <35565271+nickhan123@users.noreply.github.com>
2021-03-22 20:44:28 +0000

    fixed unable to pip install

--- readme.md
+++ readme.md
@@ -1,25 +1,8 @@
-## Here is the following structure for the repository
+## Intro
 
-* Tessellation - Killick's retina generator
-* bin - RetinaCUDA library developed by Balog
-* examples - examples provided in Ozimek's code
-* retinavision - Ozimek's code
-* retinavision_cython - Cythonised version of Ozimek's code, mainly on the retina sampling algorithm (retinavision_cython/retina/retina_sample.pyx)
-* data.csv - a 1920 x 1080 array of uint8 datatype for testing purposes (baseline_image)
-* setup.py - required for compiling .pyx files, command can be found below
-* test_retina_cython.py - testing/benchmarking takes places here
+## Purpose
 
-### Notes
-
-* generated files (.pyd and .c) from compiling .pyx files have been added into gitignore
-* .html and .cpp files have been added to gitignore and older versions in the repository are no longer tracked
-* .pkl files have been removed from gitignore
-* repository visibility is now public
-
-### Compiling .pyx code
-
-* Cython version 0.29.21
-* navigate to base directory, $python setup.py build_ext --inplace
+To optimise the existing retina generation and sampling methods
 
 
 ### Sources:
--- setup.py
+++ setup.py
@@ -1,32 +1,52 @@
 from Cython.Build import cythonize
-from setuptools import setup
-from distutils.core import setup, Extension
+from setuptools import setup, Extension, find_packages
+# from distutils.core import setup, Extension
 import numpy
 
 ext_modules = [
-    Extension(
-        "src.software_retina.utils", 
-        ["src/software_retina/utils.pyx"],
-        extra_compile_args=['-fopenmp'],
-        extra_link_args=['-fopenmp'],
-    ),
+    # Extension(
+    #     "software_retina.utils", 
+    #     ["src/software_retina/utils.pyx"],
+    #     extra_compile_args=['-fopenmp'],
+    #     extra_link_args=['-fopenmp'],
+    #     include_dirs=[numpy.get_include()],
+    # ),
     Extension(
         "src.software_retina.retina",
         ["src/software_retina/retina.pyx"],
         extra_compile_args=['-fopenmp'],
         extra_link_args=['-fopenmp'],
-        include_dirs=['pxd'],
+        include_dirs=[numpy.get_include()],
+
     ),
     Extension(
         "src.software_retina.rf_generation",
         ["src/software_retina/rf_generation.pyx"],
+        include_dirs=[numpy.get_include()],
     ),
 ]
 
 setup(
-    name="software_retina",
+    name='software-retina',
+    version='0.0.4',
+    author='Han M. Loo',
+    author_email='nloo33755@gmail.com',
+    packages=['src', 'src.software_retina', 'src.software_retina_generation'],
+    package_data={'src.software_retina': ['*.pyx', '*.pxd']},
+    url='https://github.com/hanl00/software-retina',
+    description='A software retina inspired by the biological vision system',
+    long_description=open('README.md').read(),
     ext_modules=cythonize(ext_modules),
-    include_dirs=[numpy.get_include()]
+    classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Cython",
+        "Programming Language :: Python :: Implementation :: CPython",
+    ],
+    zip_safe=False,
 )
 
 # python setup.py build_ext --inplace
